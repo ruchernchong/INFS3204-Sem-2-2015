76d654f7174aa9dc4390e2ea25515c9888265941
Ru Chern <iruchern@gmail.com>
2015-08-11 23:46:19 +1000

    Added comments to coding as explanation.

--- Prac 1/Prac 1/Prac 1/index.aspx.cs
+++ Prac 1/Prac 1/Prac 1/index.aspx.cs
@@ -11,6 +11,7 @@ namespace Prac_1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Prevent page from listing the operations again after a calculation was made. If such, "+" would be used despite operators selection.
             if (!IsPostBack)
             {
                 var operators = new List<string> {
@@ -19,10 +20,12 @@ namespace Prac_1
                 "*",
                 "/"
             };
+                // Bind the operators to the dropdownlist.
                 dropOperators.DataSource = operators;
                 dropOperators.DataBind();
             }
 
+            // OnLoad, assign text to the controls.
             lblBase10.Text = "=Base10: ";
             lblBase2.Text = "=Base2: ";
             lblNumOfZero.Text = "Num of 0";
@@ -30,6 +33,7 @@ namespace Prac_1
             btnCalculate.Text = "Calculate";
             btnCount.Text = "Count";
 
+            // Result box should not be user editable. Disabling it will prevent that from happening.
             txtResultBase10.Enabled = false;
             txtResultBase2.Enabled = false;
             txtNumOfZero.Enabled = false;
@@ -38,16 +42,19 @@ namespace Prac_1
 
         protected void btnCalculate_Click(object sender, EventArgs e)
         {
+            // Run method.
             calculateResult();
         }
 
         protected void btnCount_Click(object sender, EventArgs e)
         {
+            // Run method.
             countZeroAndOne();
         }
 
         private void calculateResult()
         {
+            // Validation input; empty input handler message. \
             if (txtInputOne.Text == "" || txtInputTwo.Text == "")
             {
                 string emptyInputs = "alert(\"Either Input One or Input Two is empty. Please enter a valid number for each field.\");";
@@ -58,6 +65,7 @@ namespace Prac_1
             {
                 try
                 {
+                    // Selection of operators using index value will determine the results are calculated.
                     switch (dropOperators.SelectedIndex)
                     {
                         case 0:
@@ -84,6 +92,7 @@ namespace Prac_1
                 }
                 catch (FormatException e)
                 {
+                    // Catch format error if input is a string instead of an integer.
                     string FormatError = "alert(\"Please ensure that the input you entered is a valid integer.\");";
                     ScriptManager.RegisterStartupScript(this, GetType(),
                         "ServerControlScript", FormatError, true);
@@ -93,6 +102,7 @@ namespace Prac_1
 
         private void countZeroAndOne()
         {
+            // Handles null input in calculated Base2 result box. Will prevent counting of 1 and 0 from running.
             if (txtResultBase2.Text == "")
             {
                 txtNumOfZero.Text = "Counter detected null input.";
@@ -100,6 +110,7 @@ namespace Prac_1
             }
             else
             {
+                // Runs counting of 1 and 0.
                 int countZero = txtResultBase2.Text.Split('0').Length - 1;
                 int countOne = txtResultBase2.Text.Split('1').Length - 1;
 
