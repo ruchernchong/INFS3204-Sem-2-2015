e256716b4b08e5cf07f3c221cf6df3f771862845
Ru Chern <iruchern@gmail.com>
2015-08-27 10:38:37 +1000

    Fix array

--- Prac 2/Zodiac Finder/Zodiac Finder/webSvc_NameByDate.asmx.cs
+++ Prac 2/Zodiac Finder/Zodiac Finder/webSvc_NameByDate.asmx.cs
@@ -14,22 +14,37 @@ namespace Zodiac_Finder
     {
         string[,] zodiac = 
             {
-                {"08", "02", "Leo"},
-                {"08", "30", "Virgo"},
-                {"09", "25", "Libra"},
-                {"11", "08", "Scorpio"},
-                {"11", "30", "Sagittarius"},
-                {"12", "25", "Capricorn"}
+                {"Aquarius", "01", "21", "02", "19"},
+                {"Pisces", "02", "20", "03", "20"},
+                {"Aries", "03", "21", "04", "20"},
+                {"Taurus", "04", "21", "05", "21"},
+                {"Gemini", "05", "22", "06", "21"},
+                {"Cancer", "06", "22", "07", "22"},
+                {"Leo", "07", "23", "08", "22"},
+                {"Virgo", "08", "23", "09", "23"},
+                {"Libra", "09", "24", "10", "23"},
+                {"Scorpio", "10", "24", "11", "22"},
+                {"Sagittarius", "11", "23", "12", "21"},
+                {"Capricorn", "12", "22", "01", "20"}
             };
 
         [WebMethod]
-        public string FindZodiacByDate(int mon, int day)
+        public string FindZodiacByDate(int mth, int day)
         {
+            DateTime inputDate = new DateTime(2012, mth, day);
+            Debug.WriteLine(inputDate);
             for (int i = 0; i < zodiac.GetLength(0); i++)
             {
-                if (int.Parse(zodiac[i, 0]) >= mon && int.Parse(zodiac[i, 1]) >= day)
+                DateTime startDate = new DateTime(2012, int.Parse(zodiac[i, 1]), int.Parse(zodiac[i, 2]));
+                DateTime endDate = new DateTime(2012, int.Parse(zodiac[i, 3]), int.Parse(zodiac[i, 4]));
+
+                Debug.WriteLine(startDate);
+
+                if (inputDate >= startDate && inputDate <= endDate)
                 {
-                    return zodiac[i, 2];
+                    Debug.WriteLine(zodiac[i, 0]);
+                    return zodiac[i, 0];
+
                 }
             }
             return "Wrong Input Date";
