294576ecf7570224c5baea05f0fb7f8021a3d674
Ru Chern <iruchern@gmail.com>
2015-09-25 03:30:19 +1000

    Implement possible method

--- Prac 3/BookStore/BookStore/BookPurchaseService.svc.cs
+++ Prac 3/BookStore/BookStore/BookPurchaseService.svc.cs
@@ -11,8 +11,67 @@ namespace BookStore
     // NOTE: In order to launch WCF Test Client for testing this service, please select BookPurchaseService.svc or BookPurchaseService.svc.cs at the Solution Explorer and start debugging.
     public class BookPurchaseService : IBookPurchaseService
     {
-        public void DoWork()
+        private static string path = HttpRuntime.AppDomainAppPath;
+        private static string file = @"books.txt";
+        private string finalPathname = path + file;
+
+        //private string tempFile = Path.GetTempFileName();
+
+        public BookPurchaseInfo PurchaseBook(BookPurchaseResponse Response)
         {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<int, int> Books(int input)
+        {
+            Dictionary<int, int> Books = new Dictionary<int, int>();
+
+            String[] readerBooks = ReadLines().ToArray();
+            String[] delimiters = {
+                                      ",",
+                                      "\r\n"
+                                  };
+
+            for (int i = 0; i < readerBooks.GetLength(0); i++)
+            {
+                String[] arrayBooks = readerBooks[i].Split(delimiters,
+                    StringSplitOptions.RemoveEmptyEntries)
+                    .Select(Book => Book.Trim())
+                    .ToArray();
+
+                Books.Add((i + 1), input);
+            }
+
+            return Books;
+
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<String> ReadLines()
+        {
+            StreamReader readerBooks;
+            string line;
+
+            try
+            {
+                readerBooks = new StreamReader(finalPathname);
+            }
+            catch (Exception Ex)
+            {
+                throw new FaultException<Exception>(new Exception(Ex.Message));
+            }
+
+            while ((line = readerBooks.ReadLine()) != null)
+            {
+                yield return line;
+                Debug.WriteLine(line);
+            }
+            readerBooks.Close();
+        }
+
+        public BookPurchaseInfo BookPurchaseInfo()
+        {
+            throw new NotImplementedException();
         }
     }
 }
